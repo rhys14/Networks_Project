9fab7de8bffb63806bbde9ae8793e1338df52a8d
rhit_steubees <steubees@rose-hulman.edu>
2026-05-08 10:57:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+/*
 
+Plan: 
+- Star topology
+- use Crow and Websockets
+- serve html webpage for users to connect and 
+- On connection, clients download host's audio file
+- Host sends JSON packet with commands and timing data
+- Clients estimate delay to sync and play
+
+Name ideas:
+- Star Ensemble
+- Chorus
+- MusicCircle
+- Sync
+
+*/
