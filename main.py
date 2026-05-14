22c812af4c74c67e4084d455a50beea98f62ef38
rhit_steubees <steubees@rose-hulman.edu>
2026-05-13 21:39:23 -0400

    Basic HTTP server
    Not really sure how to develop from here...

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import socket
+import sys
+import os
+import signal
+import http.server
+import socketserver
 
+
+if len(sys.argv) < 2:
+    port = int(input("Enter port number:"))
+else:
+    port = int(sys.argv[1])
+
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", port), handler) as server:
+    print(f"Server started at http://localhost:{port}")
+    server.serve_forever()
